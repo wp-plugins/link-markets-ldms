df291663aadc600f29ffd1d855974a9ecd8442d2
linkmarket <linkmarket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-03 16:14:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/link-markets-ldms/trunk@340361 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ main.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: Link Market's LDMS
+Plugin URI: http://www.linkmarket.com/customer_center/index.php?action=show&cat=16
+Description: The Link Market's LDMS (Link Directory Management Service) plug-in will manage all the links that have been traded through LinkMarket.com websites. 
+Version: The Plugin's Version Number, e.g.: 1.0.1
+Author: Link Market Team
+Author URI: http://www.linkmarket.com/
+License: A "Slug" license name e.g. GPL2
+*/
+?>
+<?php
+/*  Copyright 2010  Link Market Team  (email : support@linkmarket.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+
+function GetLDMS($text)
+{
+	$mykey_values = get_post_custom_values('ldmskey');
+	
+	if(trim($mykey_values[0]) =="")
+	{
+		$ldms_html_code ="<hr /><b>Warning: LDMS Key is not valid!</b><hr />";
+	}
+	
+	$url = "http://api.linkmarket.com/mng_dir/get_links.php?user_id=".trim($mykey_values[0])."&cid=".$_GET['cid']."&start=".$_GET['start']."";
+		
+	$ldms_html_code .= GetLDMSHtmlCode($url);	
+	
+	$text = str_replace('[linkmarketldms]',$ldms_html_code, $text);
+			
+	return $text;
+}
+
+function GetLDMSHtmlCode($url)
+{
+	$buffer = ""; 
+	$urlArr = parse_url($url);
+	if($urlArr[query])
+		{$urlArr[query] = "?".$urlArr[query];}
+	$fp = fsockopen($urlArr[host], 80, $errno, $errstr, 30);
+	if (!$fp)
+	{
+		echo "$errstr ($errno)<br />\n";
+	}
+	else
+	{
+		$out = "GET /".substr($urlArr[path], 1).$urlArr[query]." HTTP/1.0\r\n";
+		$out .= "Host: ".$urlArr[host]."\r\n";
+		$out .= "Connection: Close\r\n\r\n";
+		fwrite($fp, $out);
+		while (!feof($fp))
+		{
+			$buffer .= fgets($fp, 128);
+		} 
+		fclose($fp);
+	}
+	
+	$buffer = strstr($buffer,"\r\n\r\n");
+	return $buffer;
+}
+
+add_filter('the_content','GetLDMS');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Link Market's LDMS ===
+Contributors: Link Market Team
+Tags: link market, linkmarket, link building, link exchange, link directory, ldms, link trade
+Requires at least: 2.7.2
+Tested up to: 3.0.4
+Stable tag: 1.0.1
+
+== Description ==
+
+The Link Market's LDMS (Link Directory Management Service) plug-in will manage all the links that have been traded through LinkMarket.com website.
+
+
+= Features =
+
+• Broken Links will be automatically removed from your directory 
+• The links will be added in the correct category as they are on LM site 
+• You will not have more than 25 links per page 
+• No more FTP uploads to your server 
+• Multi-Verify Feature if you have multiple link requests in the same category that needs to be verified, we will verify up to 25 of them at the same time 
+• With a Add your Site link if someone wants to trade links with you and they are not LM user we will give them an option to sign up and send you automatic link request 
+• Customize your directory using cascading style sheets
+
+
+== Installation ==
+
+Follow the steps below to install the plugin.
+
+1. To download plugin please, click here .
+2. Upload Link Market's LMDS plugin to the /wp-content/plugins/ directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Create new page (My Link Page) and add the text [linkmarketldms] in the text box
+5. In the new page that was just created add a new custom field:
+	- Name: ldmskey
+	- Value: is on the Edit Link Directory Management Service page when you login to LinkMarket.com
+6. On the Edit Link Directory Management Service page, update the URL to the your link page to the url of the new page that was just created 
+
+
+== Help ==
+
+For help and support please refer to the LinkMarket's help section at <a href="http://www.linkmarket.com/customer_center/index.php?action=show&cat=16">LDMS</a>.
+
+== Changelog ==
+
+= 1.0.1 =
+
+* Inital Lunch of the plugin
+
